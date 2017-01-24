04ef7e1fc1aa8ea468b1fb2004175fd10b67cd3a
MartinPie <martinpierre.pro@gmail.com>
2017-01-24 16:57:36 +0100

    First Commit

---
+++ src/main/java/BRUN_MARTIN/Jeu_de_lettres/App.java
@@ -0,0 +1,13 @@
+package BRUN_MARTIN.Jeu_de_lettres;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
